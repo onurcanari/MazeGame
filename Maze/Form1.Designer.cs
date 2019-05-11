01e5b1becd58d29a48d997c35f3e0d5c7325f23d
onurcanari <arionurcan@gmail.com>
2019-05-11 03:01:22 +0300

    Add project files.

---
+++ Maze/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Maze {
+    partial class World {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing&&(components!=null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.character = new System.Windows.Forms.PictureBox();
+            this.wall = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.character)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wall)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // character
+            // 
+            this.character.BackColor = System.Drawing.Color.Firebrick;
+            this.character.Location = new System.Drawing.Point(40, 0);
+            this.character.Name = "character";
+            this.character.Size = new System.Drawing.Size(40, 40);
+            this.character.TabIndex = 0;
+            this.character.TabStop = false;
+            // 
+            // wall
+            // 
+            this.wall.BackColor = System.Drawing.Color.LightGray;
+            this.wall.Location = new System.Drawing.Point(0, 80);
+            this.wall.Name = "wall";
+            this.wall.Size = new System.Drawing.Size(40, 40);
+            this.wall.TabIndex = 1;
+            this.wall.TabStop = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(514, 276);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(200, 100);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // World
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 961);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.wall);
+            this.Controls.Add(this.character);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.KeyPreview = true;
+            this.Name = "World";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.World_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.character)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wall)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox character;
+        private System.Windows.Forms.PictureBox wall;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
---
+++ Maze/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Maze {
+    public partial class World : Form {
+
+        public const int MOVING_SPEED = 40;
+
+        Point newPoint;
+        
+        public World() {
+            InitializeComponent();
+        }
+        
+      
+
+        private void World_KeyDown(object sender, KeyEventArgs e) {
+            
+            newPoint = character.Location;
+
+            if(e.KeyCode == Keys.Up) {
+                
+                newPoint.Y-=MOVING_SPEED;
+            }
+            if(e.KeyCode == Keys.Down) {
+                newPoint.Y+=MOVING_SPEED;
+            }
+            if(e.KeyCode == Keys.Left) {
+                newPoint.X-=MOVING_SPEED;
+            }
+            if(e.KeyCode == Keys.Right) {
+                newPoint.X+=MOVING_SPEED;
+            }
+            
+            character.Location = newPoint;
+        }
+
+        private void World_KeyPress(object sender, KeyPressEventArgs e) {
+
+        }
+    }
+}
